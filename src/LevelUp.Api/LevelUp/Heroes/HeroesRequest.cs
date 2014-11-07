8733a715796078bfb854ce98d8c915f4561972df
Cliff Burger <cliffburger@daptiv.com>
2014-11-07 14:43:13 -0800

    Add heroes stuff

---
+++ src/LevelUp.Api/LevelUp/Heroes/HeroesRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ServiceStack;
+
+namespace LevelUp.Api.LevelUp.Heroes
+{
+    [Route("/heroes", Verbs = "GET")]
+    public class HeroesRequest: IReturn<IEnumerable<Hero>>
+    {
+        
+    }
+}
