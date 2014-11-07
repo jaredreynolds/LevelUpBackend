1e2004bbb52973b090798298b7f81fd16816d6b9
Cliff Burger <cliffburger@daptiv.com>
2014-11-07 10:13:06 -0800

    Achievements handler stub works

---
+++ src/LevelUp.Api/LevelUp/Achievements/AchievementsService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ServiceStack;
+
+namespace LevelUp.Api.LevelUp.Achievements
+{
+    public class AchievementsService: IService
+    {
+        public object Get(AchievementsRequest request)
+        {
+            return new List<Achievement>
+                {
+                    new Achievement
+                        {
+                            AchievementId = 1,
+                            Title = "Create an API!",
+                            Points = 12,
+                            Description = "Using Service Stack1"
+                        },
+                      new Achievement
+                        {
+                            AchievementId = 2,
+                            Title = "Use an API!",
+                            Points = 15,
+                            Description = "Using ng resource!"
+                        }  
+                };
+        }
+    }
+}
