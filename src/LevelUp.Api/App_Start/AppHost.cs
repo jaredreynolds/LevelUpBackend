b8b5656e2786c18abb4b4d7698414f869f990f91
Cliff Burger <cliffburger@daptiv.com>
2014-11-07 13:13:28 -0800

    Enable cross origin requests
    So that our UI can call this.

--- src/LevelUp.Api/App_Start/AppHost.cs
+++ src/LevelUp.Api/App_Start/AppHost.cs
@@ -14,34 +14,43 @@ using ServiceStack;
 
 namespace LevelUp.Api.App_Start
 {
-	public class AppHost : AppHostBase
-	{		
-		public AppHost() //Tell ServiceStack the name and where to find your web services
-			: base("StarterTemplate ASP.NET Host", typeof(AchievementsService).Assembly) { }
+    public class AppHost : AppHostBase
+    {
+        public AppHost() //Tell ServiceStack the name and where to find your web services
+            : base("StarterTemplate ASP.NET Host", typeof(AchievementsService).Assembly) { }
 
-		public override void Configure(Funq.Container container)
-		{
-			//Set JSON web services to return idiomatic JSON camelCase properties
-			ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
-		
-			//Configure User Defined REST Paths
-			
-			//Uncomment to change the default ServiceStack configuration
+        public override void Configure(Funq.Container container)
+        {
+            //Set JSON web services to return idiomatic JSON camelCase properties
+            ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
+
+            base.SetConfig(new HostConfig
+                {
+                    GlobalResponseHeaders =
+                        {
+                            { "Access-Control-Allow-Origin", "*" },
+                            { "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" },
+                            { "Access-Control-Allow-Headers", "Content-Type" }
+                        }
+                });
+            //Configure User Defined REST Paths
+
+            //Uncomment to change the default ServiceStack configuration
             //SetConfig(new HostConfig {
             //});
 
-			//Enable Authentication
-			//ConfigureAuth(container);
+            //Enable Authentication
+            //ConfigureAuth(container);
 
-			//Register all your dependencies
-		    container.Register(new LevelUpContext());
+            //Register all your dependencies
+            container.Register(new LevelUpContext());
             container.RegisterAutoWired<AchievementsRepository>();
             container.RegisterAutoWired<AchievementsService>();
-		}
+        }
 
-		public static void Start()
-		{
-			new AppHost().Init();
-		}
-	}
+        public static void Start()
+        {
+            new AppHost().Init();
+        }
+    }
 }
